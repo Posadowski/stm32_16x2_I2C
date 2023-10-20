980911907387aa08a295d981ae048196558f6cdc
Posadowski <mateuszposa@gmail.com>
2023-10-21 01:03:59 +0200

    feat: add functions to display dds18b20 data

--- Core/Inc/i2c-lcd.h
+++ Core/Inc/i2c-lcd.h
@@ -1,5 +1,6 @@
-#include "stm32f4xx_hal.h"
-
+#ifndef I2C_LCD_H
+#define I2C_LCD_H
+#include "stdint.h"
 void lcd_init (void);   // initialize lcd
 
 void lcd_send_cmd (char cmd);  // send command to the lcd
@@ -8,6 +9,11 @@ void lcd_send_data (char data);  // send data to the lcd
 
 void lcd_send_string (char *str);  // send string to the lcd
 
-void lcd_put_cur(int row, int col);  // put cursor at the entered position row (0 or 1), col (0-15);
+void lcd_put_cur(uint8_t row, uint8_t col);  // put cursor at the entered position row (0 or 1), col (0-15);
 
 void lcd_clear (void);
+
+void lcd_display_ds18b20(float temperature);
+
+void lcd_display_ds18b20_count(uint8_t ds18b20_sensor_count);
+#endif // I2C_LCD_H
--- Core/Src/i2c-lcd.c
+++ Core/Src/i2c-lcd.c
@@ -2,6 +2,7 @@
 /** Put this in the src folder **/
 
 #include "i2c-lcd.h"
+#include "main.h"
 extern I2C_HandleTypeDef hi2c2;  // change your handler here accordingly
 
 #define SLAVE_ADDRESS_LCD 0x4E // change this according to ur setup
@@ -41,7 +42,7 @@ void lcd_clear (void)
 	}
 }
 
-void lcd_put_cur(int row, int col)
+void lcd_put_cur(uint8_t row, uint8_t col)
 {
     switch (row)
     {
@@ -85,5 +86,43 @@ void lcd_init (void)
 
 void lcd_send_string (char *str)
 {
-	while (*str) lcd_send_data (*str++);
+    int length = 0;
+    //Calculate the length of the string
+    while (*str) {
+        length++;
+        str++;
+        // Check if the length exceeds 15 characters
+        if (length > 15) {
+      	    lcd_clear();
+      	    lcd_put_cur(0,0);
+        	lcd_send_string("string too long");
+        	HAL_Delay(1000);
+            return;
+        }
+    }
+    str -= length;  // Restore the pointer to the beginning of the string
+	while (*str) {
+		lcd_send_data (*str++);
+	}
+}
+
+void lcd_display_ds18b20(float temperature){
+	uint8_t integer_part = (uint8_t)temperature;  // Get the integer part
+	uint8_t  decimal_part = (uint8_t)((temperature - integer_part) * 100);  // Get the decimal part (multiplied by 100 to get two digits)
+	char ds18b20_data_to_send[16];
+	lcd_put_cur(1,0);
+	memset(ds18b20_data_to_send, 0, sizeof(ds18b20_data_to_send));
+	snprintf(ds18b20_data_to_send, sizeof(ds18b20_data_to_send), "temp: %u.%u", integer_part,decimal_part);
+	lcd_send_string(ds18b20_data_to_send);
+}
+
+void lcd_display_ds18b20_count(uint8_t ds18b20_sensor_count){
+	  lcd_clear();
+	  lcd_put_cur(0,0);
+	  char sensor_count_string[16];
+	  snprintf(sensor_count_string, sizeof(sensor_count_string), "Detected %u",ds18b20_sensor_count);
+	  lcd_send_string(sensor_count_string);
+
+	  lcd_put_cur(1,0);
+	  lcd_send_string("DS18B20 sensors");
 }
